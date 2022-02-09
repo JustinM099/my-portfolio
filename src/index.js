52e2f1a5e84f335cb49c349ad58c2c17db819214
JustinM099 <justinm099@mac.com>
2022-02-08 23:23:52 -0800

    working on making react router work with github pages

--- src/index.js
+++ src/index.js
@@ -23,7 +23,7 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router basename="/">
+    <Router basename="/my-portfolio">
       <Routes>
         <Route exact path="/" element={<App />}>
           <Route index element={<Intro />} />
