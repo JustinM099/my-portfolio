db04141dd2df489edb1a3142ddf2700736314c6d
JustinM099 <justinm099@mac.com>
2022-02-10 15:34:57 -0800

    working on responsiveness

--- src/components/About.js
+++ src/components/About.js
@@ -11,7 +11,7 @@ const About = () => {
                 <div className="card z-depth-5">
                     <section className="about">
                         <div className="profile" id="profile">
-                            <img src={require('../assets/images/profile-image-2021.png')} alt="my profile image"
+                            <img src={require('../assets/images/profile-image-circle.png')} alt="my profile image"
                                 className="profile-pic" />
                             <h2>Hello!</h2>
                             <p>I'm Justin Meredith, a wine nerd turned web developer. I've spent more than a decade in the wine industry.
