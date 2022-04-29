dd2b5f4b2504483c2ad21f8b598493a11a914d97
JustinM099 <justinm099@mac.com>
2022-04-28 23:28:14 -0700

    updating contact page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -36,7 +36,7 @@ const Contact = () => {
         exit={{ opacity: 0 }}
         transition={{ duration: 0.5 }}
     >
-        <div className="center content contact-content">
+        <div className="center content">
             <div className="card z-depth-5">
 
                 <section className="contact-me" id="contact">
@@ -44,23 +44,9 @@ const Contact = () => {
                     <h3>If you want to talk, please reach out.</h3>
                     <p>Love wine? Love spirits? Love web development? Let's drink and develop together!</p>
 
-                    <Container className="container form-container">
-                        <form className="row form" id="email-form">
-                            <div className="input-field s12 m6 form-control">
-                                <input id="name" name="name" type="text" placeholder="Your Name" className="validate" onBlur={validate} />
-                            </div>
-                            <div className="input-field s12 m6 form-control">
-                                <input id="email" name="email" type="email" placeholder="Your Email" className="validate" onBlur={validateEmailInput} />
-                            </div>
-                            <div className="input-field s12 m6 form-control">
-                                <textarea rows="5" id="name" type="text" placeholder="Your Note To Me" className="validate" onBlur={validate} />
-                                <button className="BUTTON_EVA form-control" type="submit" onSubmit={submitButton}>SEND</button>
-                            </div>
-                        </form>
-                    </Container>
                     <div className="button-container">
                         <a className="BUTTON_EVA" href="./assets/Justin Meredith - Resume.pdf">MY RESUME</a>
-
+                        <a className="BUTTON_EVA" href="mailto:justinm099@mac.com">EMAIL ME</a>
 
                         <a className="BUTTON_EVA"
                             href="tel:12068615635"
